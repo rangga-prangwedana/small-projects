a859977d32da1e1270761ef15f2e31e35d72674a
Rangga Prangwedana <vergil1st@gmail.com>
2025-10-26 19:04:44 +0700

    Finished password generator using Python

--- password-generator/pass-generator.py
+++ password-generator/pass-generator.py
@@ -1,19 +1,45 @@
+import string
+from secrets import choice
+
 def password_gen(length:int):
-    pass
+    """
+    Returns generated password based on passed length variable.
+    """
+    uppercase = string.ascii_uppercase
+    lowercase = string.ascii_lowercase
+    number = string.digits
+    symbols = "!@#$%^&*()_+-=[]{},.<>/?"
+
+    all_chars = uppercase + lowercase + number + symbols
+
+    password = "".join(choice(all_chars) for _ in range(length))
+
+    return password
 
 
-def check_length(length: int):
-    if not (8 <= length <= 24):
-        raise ValueError("Number must be between 8 and 24, inclusive.")
+def check_length(length: int, min_value:int, max_value:int):
+    """
+    Check length if is in the range of min_value and max_value.
+    """
+    if not (min_value <= length <= max_value):
+        raise ValueError(f"Number must be between {min_value} and {max_value}, inclusive.")
     return length
 
 
+MIN_VALUE = 8
+MAX_VALUE = 24
+
 def main():
-    number = int(input("Input the length of the desired password (8-24):"))
-    try:
-        length = check_length(number)
-    except ValueError as e:
-        print(f"Error: {e}")
+    while True:
+        number = int(input(f"Input the length of the desired password ({MIN_VALUE}-{MAX_VALUE}): "))
+        try:
+            length = check_length(number, MIN_VALUE, MAX_VALUE)
+            break
+        except ValueError as e:
+            print(f"Error: {e}")
+
+    
+    print(f"Generated password: {password_gen(length)}")
 
 
 
